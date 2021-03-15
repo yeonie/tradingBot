b4a7194c59c022c2bb185440a51a61ef2e78d8ed
litong <litong4379@gmail.com>
2021-03-15 15:12:12 +0900

    betaVersion1.0.1
    처음실행했을때의 시세 차익 텔레그램과 콘솔 창에 print / alert.
    
    err. 텔레그램에 실행시킨 이후의 목록이 한꺼번에 전부 감

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const request = require('request')
 const uuidv4 = require("uuid/v4")
 const sign = require('jsonwebtoken').sign
-//시간반복
-var schedule = require('node-schedule');
-var rule = new schedule.RecurrenceRule();
-rule.minutes = 1;
+
 //매 시간 n분 마다 수행
 // var j = schedule.scheduleJob(rule, function(){
 //          console.log('1분지났다');
@@ -13,16 +10,6 @@ var PrePrice = 777;
 global.PrePrice = PrePrice;
 var CurPrice = 5959;
 global.CurPrice = CurPrice;
-
-
-fetch(btcMinurl, BTCoptions, bot)
-.then(res => res.json())
-.then(data => {
-      var btcPriceData = data;
-      console.log(data[0]);
-
-
-PrePrice = data[0].trade_price;
 
 
 // var j = schedule.scheduleJob(rule, function(){
@@ -112,77 +99,120 @@ const BTCoptions = {method: 'GET', qs: {market: 'KRW-BTC', count: '1'}};
 fetch(btcMinurl)
   .then(res => res.json())
   .then(data => {
-    global.PrePrice = data[0].trade_price;
-    console.log("함수 안 현재가격 : " + global.PrePrice);
+    PrePrice = data[0].trade_price;
+    //여기서부터 현재가격 계속 import 3분마다 체크
+    //시간반복
+    var schedule = require('node-schedule');
+    var rule = new schedule.RecurrenceRule();
+    rule.minutes = 1;
+    var j = schedule.scheduleJob(rule, function(){
+      fetch(btcMinurl)
+        .then(res => res.json())
+        .then(data => {
+          CurPrice = data[0].trade_price;
+          CurPrice = Number(CurPrice);
+          PrePrice = Number(PrePrice);
+          console.log("예전 가격 : " + PrePrice);
+          console.log("함수 안 현재가격 : " + CurPrice);
+          if(CurPrice>PrePrice){
+            var chai = parseInt(CurPrice - PrePrice);
+            console.log("처음 실행했을 때 보다 " + chai+ "원 만큼 올랐어요.");
+            if(CurPrice>=PrePrice*1.01){
+              console.log("처음 실행했을 때 보다 1%이상 올랐어요.");
+              bot.onText(/\!시(.+)/, (msg) => {
+                const chatId = msg.chat.id;
+                const resp = "처음 실행했을 때 보다 1%이상 올랐어요.";
+                bot.sendMessage(chatId, resp);
+              });
+            }
+            else{
+              bot.onText(/\!시(.+)/, (msg) => {
+                const chatId = msg.chat.id;
+                const resp = "처음 실행했을 때 보다 " + chai+ "원 만큼 올랐어요.";
+                bot.sendMessage(chatId, resp);
+              });
+            }
+          }
+          else{
+            var chai = parseInt(PrePrice - CurPrice);
+            chai = Number(chai);
+            console.log("처음 실행했을 때 보다 " + chai + "원 만큼 떨어졌어요");
+            if(PrePrice>=CurPrice*1.01){
+              console.log("처음 실행했을 때 보다 1%이상 떨어졌어요..");
+              bot.onText(/\!시(.+)/, (msg) => {
+                const chatId = msg.chat.id;
+                const resp = "처음 실행했을 때 보다 1%이상 떨어졌어요..";
+                bot.sendMessage(chatId, resp);
+              });
+            }else{
+              bot.onText(/\!시(.+)/, (msg) => {
+                const chatId = msg.chat.id;
+                const resp = "처음 실행했을 때 보다 " + chai + "원 만큼 떨어졌어요";
+                bot.sendMessage(chatId, resp);
+              });
+            }
+          }
+    })
+    });
+
+
     return PrePrice;
-
   })
-console.log("현재가격 : " + global.PrePrice);
-
-
-btcPrice();
-
-async function btcPrice(btcPriceData) {
-  console.log("Start Point")
-
-
-            bot.onText(/\!시(.+)/, (msg) => {
-
-
-              const fetch = require('node-fetch');
-              const BTCoptions = {method: 'GET', qs: {market: 'KRW-BTC', count: '1'}};
-
-              fetch(btcMinurl, BTCoptions, bot)
-              .then(res => res.json())
-              .then(data => {
-                    var btcPriceData = data;
-                    console.log(data[0]);
-
-
-            CurPrice = data[0].trade_price;
-
-            console.log(CurPrice);
-              // console.log("여기는 들어와졌니?"); //들어와.
-              const chatId = msg.chat.id;
-
-              const resp = "비트코인의 가격이 3%나 상승했어요!"; // the captured "whatever"
-              bot.sendMessage(chatId, resp);
-            });
-
-        })
-  .catch(err => console.error('error:' + err));
-
-  return data[0].trade_price;
-
-}
-
-//함수 처리
-var j = schedule.scheduleJob(rule, function(){
-
-  btcPrice();
-
-         console.log('가격비교');
-         if(PrePrice < CurPrice){
-           console.log('1분전보다 가격이 올랐어요');
-           console.log('이전가격 : ' + PrePrice);
-           console.log('지금가격 : ' + CurPrice);
-         }else if(PrePrice == CurPrice){
-           console.log("아무일도 없었다.");
-         }
-         else{
-           console.log('1분전보다 가격이 내렸어요');
-           console.log('이전가격 : ' + PrePrice);
-           console.log('지금가격 : ' + CurPrice);
-         }
-});
+  //함수 처리
+  // var j = schedule.scheduleJob(rule, function(){
+  //
+  //   btcPrice();
+  //
+  //          console.log('가격비교');
+  //          if(PrePrice < CurPrice){
+  //            console.log('1분전보다 가격이 올랐어요');
+  //            console.log('이전가격 : ' + PrePrice);
+  //            console.log('지금가격 : ' + CurPrice);
+  //          }else if(PrePrice == CurPrice){
+  //            console.log("아무일도 없었다.");
+  //          }
+  //          else{
+  //            console.log('1분전보다 가격이 내렸어요');
+  //            console.log('이전가격 : ' + PrePrice);
+  //            console.log('지금가격 : ' + CurPrice);
+  //          }
+  // });
 
 
 
-
-
-
-
-
+// async function btcPrice(btcPriceData) {
+//   console.log("Start Point")
+//
+//
+//             bot.onText(/\!시(.+)/, (msg) => {
+//
+//
+//               const fetch = require('node-fetch');
+//               const BTCoptions = {method: 'GET', qs: {market: 'KRW-BTC', count: '1'}};
+//
+//               fetch(btcMinurl, BTCoptions, bot)
+//               .then(res => res.json())
+//               .then(data => {
+//                     var btcPriceData = data;
+//                     console.log(data[0]);
+//
+//
+//             CurPrice = data[0].trade_price;
+//
+//             console.log(CurPrice);
+//               // console.log("여기는 들어와졌니?"); //들어와.
+//               const chatId = msg.chat.id;
+//
+//               const resp = "비트코인의 가격이 3%나 상승했어요!"; // the captured "whatever"
+//               bot.sendMessage(chatId, resp);
+//             });
+//
+//         })
+//   .catch(err => console.error('error:' + err));
+//
+//   return data[0].trade_price;
+//
+// }
 
 
 //비동기 예제
